15f80036880954c17e76dbeb6178d829524c3990
flbaue <flbaue@posteo.de>
2014-05-22 09:40:32 +0200

    amando more activitys

--- android/Amando4/app/src/main/java/flbaue/amando4/app/ShowMap.java
+++ android/Amando4/app/src/main/java/flbaue/amando4/app/ShowMap.java
@@ -5,27 +5,25 @@
 
 package flbaue.amando4.app;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class ShowMap extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_show_map);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.show_map, menu);
         return true;
     }
 
@@ -36,25 +34,8 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
-            startActivity(new Intent(this, Settings.class));
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    public void btnClickSendPosition(View view) {
-        startActivity(new Intent(this, SendPosition.class));
-    }
-
-    public void btnClickManageGeoContacts(View view) {
-        startActivity(new Intent(this, ManageGeoContacts.class));
-    }
-
-    public void btnClickShowMap(View view) {
-        startActivity(new Intent(this, ShowMap.class));
-    }
-
-    public void btnClickStartSimulation(View view) {
-        startActivity(new Intent(this, StartSimulation.class));
     }
 }
